48ba488e67dfb67332c60080084ba7ae4670a31b
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-09-23 22:05:18 -0500

    atualizando

--- Projeto-java/src/executaveis/PrimeirClass.java
+++ Projeto-java/src/executaveis/PrimeirClass.java
@@ -2,9 +2,14 @@ package executaveis;
 
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import curso.java.Classesauxiliares.FuncaoAtenticacao;
+import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
 
 public class PrimeirClass {
@@ -12,23 +17,48 @@ public class PrimeirClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try {
+		
 		String login = JOptionPane.showInputDialog("Informe o login:");
 		String senha = JOptionPane.showInputDialog("Informe a senha:");
 				 
-	
-	
-		
+
 		
 		if (new FuncaoAtenticacao (new Diretor(login, senha)).autenticar()) { /*Vou traavar o contrato para autorizar somente quem realmente tem o contrato 100% legitimo*/
 			
+			List<Aluno> alunos = new ArrayList<Aluno>();
+		
 		
 		}else {
 			JOptionPane.showMessageDialog(null,"Acesso não permitido");
 		}
-		
+		/*aqui*/
+		}catch(Exception e) {
+			
+			StringBuilder saida = new StringBuilder()
+;			
+			e.printStackTrace();/*Imprime erro no console Java*/
+			
+			for (int pos = 0; pos <= e.getStackTrace().length; pos++) {
+				
+				saida.append("\n Classe de erro: " +e.getStackTrace()[pos].getClassName() );
+				saida.append("\n Método de erro: " +e.getStackTrace()[pos].getMethodName() );
+				saida.append("\n linha de erro: " +e.getStackTrace()[pos].getLineNumber() );
+				saida.append("\n Class: " +e.getClass().getName());
+			}	
+				
+			
+			JOptionPane.showConfirmDialog(null, "Erro ao processar notas" + saida.toString()); 
+		} catch (NullPointerException e) {
+			JOptionPane.showConfirmDialog(null, "Opaa um null pointer exeption : " + e.getClass());
 		}
 		
-	}
+			 
+		}
+}
+		/**/
+		
+	
 
 
  
