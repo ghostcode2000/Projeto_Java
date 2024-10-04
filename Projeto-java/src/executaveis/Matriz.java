ce44eafcce4863e797c3101ace67e62088ae29a8
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-10-03 22:05:38 -0500

    Criando matrizes

---
+++ Projeto-java/src/executaveis/Matriz.java
@@ -0,0 +1,19 @@
+package executaveis;
+
+public class Matriz {
+ public static void main(String[] args) {
+//Isso é uma matriz	    cria uma posição com tre valores dentro dela
+	int notas[][]  = new int[2][3];
+	 
+	notas[0][0] = 80;
+	notas[0][1] = 90;
+	notas[0][2] = 70;
+	
+	notas[1][0] = 40;
+	notas[1][1] = 60;
+	notas[1][2] = 50;
+	
+	System.out.println(notas); 
+	 
+ 	}
+}
