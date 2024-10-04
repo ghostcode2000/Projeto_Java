33936456218430ff97ec7a9a2fe3b10057cc07f6
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-10-03 21:41:30 -0500

    Dominando o uso de Arrays

--- Projeto-java/src/executaveis/ArrayVetor.java
+++ Projeto-java/src/executaveis/ArrayVetor.java
@@ -29,19 +29,39 @@ public class ArrayVetor {
 		disciplina2.setNotaDisplina(notasLogica);
 		aluno.getDisciplina().add(disciplina2);
 
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Alex fernando");
+
+		// Criação da disciplina
+		Disciplina disciplina3 = new Disciplina();
+		
+		disciplina3.setNomeDisciplina("Curso Java");
+		double[] notas2 = { 8.8, 9.7, 7.6, 6.8 };
+		disciplina3.setNotaDisplina(notas2);
+		aluno2.getDisciplina().add(disciplina);
+
+		Disciplina disciplina4 = new Disciplina();
+		
+		disciplina4.setNomeDisciplina("Logica de programação");
+		double[] notasLogica2 = { 8.0, 9.7, 7.7, 6.8 };
+		disciplina4.setNotaDisplina(notasLogica2);
+		aluno2.getDisciplina().add(disciplina4);
+
 		//------------------------------------------------------------------
 		
-		Aluno[] arrayAlunos = new Aluno [1];
+		Aluno[] arrayAlunos = new Aluno [2];
 	
 		arrayAlunos[0] = aluno;
+		arrayAlunos[1] = aluno2;
 		
 		for (int pos = 0; pos < arrayAlunos.length; pos ++) {
 			System.out.println("Nome do aluno é:"+ arrayAlunos[pos].getNome());
 			
 			for (Disciplina d : arrayAlunos[pos].getDisciplina()) {
 				System.out.println("Nome da disciplina é: "+d.getNomeDisciplina());
+				
 				for (int posnota = 0;posnota < d.getNotaDisplina().length;posnota++) {
-					System.out.println("A nota númerp :" +posnota+" é igual = "+d.getNotaDisplina()[posnota]);
+					System.out.println("A nota número :" +posnota+" é igual = "+d.getNotaDisplina()[posnota]);
 				}
 			}
 		}
