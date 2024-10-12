8ba23194277c9376e3aeca032959039b3b30c740
Natã Moreira <Nat Moreira@DESKTOP-64D85T9>
2024-10-12 16:27:49 -0500

    entendendo matrizes

--- Projeto-java/src/executaveis/Matriz.java
+++ Projeto-java/src/executaveis/Matriz.java
@@ -8,12 +8,23 @@ public class Matriz {
 	notas[0][0] = 80;
 	notas[0][1] = 90;
 	notas[0][2] = 70;
+	// Uma matriz é um Array com outro Array dentro dele.
+	// o primeiro [] representa o elemento e o []replesenta os valores guardados neles.
+	// uma matriz é parecida com uma tabela de Exel.que é divida em linhas e coluna.
 	
 	notas[1][0] = 40;
 	notas[1][1] = 60;
 	notas[1][2] = 50;
 	
 	System.out.println(notas); 
-	 
+	/*Percorre as linhas*/
+	for (int poslinha = 0; poslinha < notas.length; poslinha ++) {
+		System.out.println("==============================================");
+		/*Para cada linha percorrer as colunas ARRAY*/
+		for (int poscoluna = 0; poscoluna < notas[poslinha].length; poscoluna++ ) {
+			System.out.println("Valor da matriz " + notas[poslinha][poscoluna]);
+		}
+	}
+	
  	}
 }
